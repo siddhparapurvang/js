16c32ec65235c021aebf3a3f849545d15d52f07f
siddhparapurvang <139098531+siddhparapurvang@users.noreply.github.com>
2023-09-12 11:15:12 +0530

    Create task.js

---
+++ task.js
@@ -0,0 +1,59 @@
+function first()
+{
+    document.getElementById("ok").style.backgroundColor="#e60000";
+    document.getElementById("p").style.backgroundColor="red";
+    document.getElementById("o").style.backgroundColor="red";
+
+
+
+}
+function second()
+{
+    document.getElementById("ok").style.backgroundColor="#e69500";
+    document.getElementById("p").style.backgroundColor="orange";
+    document.getElementById("o").style.backgroundColor="orange";
+
+}
+
+function third()
+{
+    document.getElementById("ok").style.backgroundColor="#ffff00";
+    document.getElementById("p").style.backgroundColor="#ffff66";
+    document.getElementById("o").style.backgroundColor="#ffff66";
+
+}
+
+function four()
+{
+    document.getElementById("ok").style.backgroundColor="#00e600";
+    document.getElementById("p").style.backgroundColor="green";
+    document.getElementById("o").style.backgroundColor="green";
+
+
+}
+
+function five()
+{
+    document.getElementById("ok").style.backgroundColor="#0000e6";
+    document.getElementById("p").style.backgroundColor="blue";
+    document.getElementById("o").style.backgroundColor="blue";
+
+
+}
+function six()
+{
+    document.getElementById("ok").style.backgroundColor="#590099";
+    document.getElementById("p").style.backgroundColor="indigo";
+    document.getElementById("o").style.backgroundColor="indigo";
+
+
+}
+
+function seven()
+{
+    document.getElementById("ok").style.backgroundColor="#ed78ed";
+    document.getElementById("p").style.backgroundColor="violet";
+    document.getElementById("o").style.backgroundColor="violet";
+
+
+}
